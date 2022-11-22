bc705ce2543b15cb8feef00d9d6d4afd0dc77beb
paulasotoglz <113448947+paulasotoglz@users.noreply.github.com>
2022-11-22 15:46:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#this is my first python program
+"""
+this is code written un python 3.8 
+interpreted by the console. 
+"""
+print ("hello world")
+print (50)
+print (3.10)
+print (50,"letters",3.10)
+print("hello", end = " ")
+print("world")
